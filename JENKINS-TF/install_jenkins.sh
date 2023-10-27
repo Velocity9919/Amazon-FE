d22d7faf92d9272c9a0c91db1c6915046a4964c7
Velocity9919 <143435067+Velocity9919@users.noreply.github.com>
2023-10-27 23:21:50 +0530

    Update install_jenkins.sh

--- JENKINS-TF/install_jenkins.sh
+++ JENKINS-TF/install_jenkins.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
-sudo apt update -y
-#sudo apt upgrade -y
-wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc
-echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
-sudo apt update -y
-sudo apt install temurin-17-jdk -y
-/usr/bin/java --version
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-                  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-                  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-                              /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update -y
-sudo apt-get install jenkins -y
-sudo systemctl start jenkins
-sudo systemctl status jenkins
-sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+  sudo apt-get update -y
+  sudo apt install openjdk-17-jre -y
+  curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+  echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+  sudo apt-get update -y
+  sudo apt-get install jenkins -y
+  sudo systemctl enable jenkins
+  sudo systemctl start jenkins
+  sudo cat /var/lib/jenkins/secrets/initialAdminPassword
 
 #install docker
+sudo apt-get update -y
+sudo apt install openjdk-11-jre -y
+
+sudo apt-get install ca-certificates curl gnupg
+
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
 sudo apt-get update
-sudo apt-get install docker.io -y
-sudo usermod -aG docker ubuntu  
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+sudo systemctl restart docker
+sudo usermod -aG docker $USER
 newgrp docker
-sudo chmod 777 /var/run/docker.sock
+sudo chmod 666 /var/run/docker.sock
+sudo systemctl restart docker
+sudo systemctl status docker
 docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
 
 # install trivy
